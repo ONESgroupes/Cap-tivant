93aaeb912cec89e34439b9ab2f09e3051fc89f5f
Paule-rcht <paro62616@eleve.isep.Fr>
2025-04-18 14:04:16 +0200

    Update info-bateau.js

---
+++ info-bateau.js
@@ -0,0 +1,29 @@
+const bateaux = [
+    {
+        id: 1,
+        titre: "Bateau Moteur - 2022",
+        image1: "images/moteur1.png",
+        image2: "images/moteur2.png",
+        personnes: "6 personnes",
+        cabines: "2 cabines",
+        longueur: "6 mètres",
+        prix: "1695€/semaine",
+        avis: "⭐ 4.7 (58)",
+        port: "📍Port de Marseille",
+        categorie: "moteur",
+    },
+    {
+        id: 2,
+        titre: "Lagoon 42 - 2020",
+        image1: "images/catamaran1.png",
+        image2: "images/catamaran2.png",
+        description: "Marseille pour 12 personnes, 21 mètres",
+        prix: "3995€/semaine",
+        avis: "⭐ 4.9 (16)",
+        port: "📍Port de Saint-Malo",
+        personnes: "12 pers",
+        cabines: "5 cabines",
+        longueur: "21m",
+        categorie: "a voile",
+    }
+];
