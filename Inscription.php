48fe08f2b47ea319aa6a555cb3b2fd5b9be229f1
huwil <huwilliam2601@gmail.com>
2025-05-07 18:26:01 +0200

    a

--- Inscription.php
+++ Inscription.php
@@ -3,7 +3,6 @@ session_start();
 require_once 'config.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
